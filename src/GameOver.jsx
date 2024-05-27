749e9485552ebaa40b0a3584ca52fa3f3ca32356
OGoncharenko <olyas.goncharenko@gmail.com>
2024-05-27 14:47:52 -0700

    Convert NumberGuessingGame and GuessControl class components into the function components

--- src/GameOver.jsx
+++ src/GameOver.jsx
@@ -1,15 +1,21 @@
 import Button from "./Button";
+import PropTypes from 'prop-types';
 
 function GameOver({ hasWon, onReset }) {
   return (
     <div>
       {hasWon && <h2>Congratulation! You guessed my number.</h2>}
       {!hasWon && (
-        <h2>You didn't guess my number. Would you like to try again?</h2>
+        <h2>You did not guess my number. Would you like to try again?</h2>
       )}
       <Button onClick={onReset}>Play Again!</Button>
     </div>
   );
 }
+
+GameOver.propTypes = {
+  hasWon: PropTypes.bool.isRequired,
+  onReset: PropTypes.func.isRequired,
+};
 
 export default GameOver;
